feb1b6fd08b1d61845adfa5d4e0f5b2fc45cb51e
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2024-09-02 04:28:18 +0300

    Finished the BMI Calulator section and changed the section spacing from padding to margin

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,41 @@ const scrollUp = () => {
                         : scrollUpArrow.classList.remove('show-scroll-up');
 };
 window.addEventListener('scroll', scrollUp);
+
+/* BMI Calculator Section */
+const calculatorForm  = document.getElementById('calculator-form');
+const heightInput  = document.getElementById('calculator-height');
+const weightInput  = document.getElementById('calculator-weight');
+const resultMassage = document.getElementById('calculator-result');
+
+const calculateBMI = (e) => {
+  e.preventDefault();
+
+  if (heightInput.value === '' || weightInput.value === '' ) {
+
+    resultMassage.textContent = 'Fill in the Height and Weight';
+
+    resultMassage.style.color = 'red';
+
+    setTimeout( () => {
+      resultMassage.textContent = '';
+    }, 3000);
+
+  } else {
+    const height = heightInput.value / 100;
+    const weight = weightInput.value;
+
+    const bmi = Math.round( weight / ( height * height ) );
+    resultMassage.style.color = 'hsl(79, 72%, 55%)';
+
+    if (bmi < 18.5) {
+      resultMassage.textContent = `Your BMI is ${bmi} and you are skinny.`;
+    } else if (bmi <= 25) {
+      resultMassage.textContent = `Your BMI is ${bmi} and you are healthy.`;
+    } else {
+      resultMassage.textContent = `Your BMI is ${bmi} and you are overweight.`;
+    }
+  }
+}
+
+calculatorForm.addEventListener('submit', calculateBMI);
